49e8cf0e5606ce7b3ba645dc0ed60b9d43f90e04
SeriousBug98 <martin1156@konkuk.ac.kr>
2024-10-16 16:28:43 +0900

    feat: 일정 기능 추가 그런데, 일정 별로 약을 더 추가하는 기능 필요

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
 const authRoutes = require("./routes/authRoutes");
+const scheRoutes = require("./routes/scheRoutes");
 const app = express();
 
 // 미들웨어 설정
 app.use(express.json());
+app.use(
+  session({
+    secret: "@chars0624",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      maxAge: 3600000,
+    },
+  })
+);
 
 // 라우트 설정
 app.use("/auth", authRoutes);
+app.use("/schedule", scheRoutes);
 
 // 서버 실행
 const PORT = process.env.PORT || 3000;
--- models/userModel.js
+++ models/userModel.js
@@ -8,14 +8,14 @@ const pool = mysql.createPool({
 });
 
 // 회원 정보를 데이터베이스에 저장
-exports.createUser = async (id, name, password) => {
-  const sql = `INSERT INTO user (id, name, password) VALUES (?, ?, ?)`;
-  await pool.execute(sql, [id, name, password]);
+exports.createUser = async (user_id, password, nickname) => {
+  const sql = `INSERT INTO user (user_id, password, nickname) VALUES (?, ?, ?)`;
+  await pool.execute(sql, [user_id, password, nickname]);
 };
 
 // 회원 정보로 사용자 조회
-exports.findUserById = async (id) => {
-  const sql = `SELECT * FROM user WHERE id = ?`;
-  const [rows] = await pool.execute(sql, [id]);
+exports.findUserById = async (user_id) => {
+  const sql = `SELECT * FROM user WHERE user_id = ?`;
+  const [rows] = await pool.execute(sql, [user_id]);
   return rows[0];
 };
